4f0ff449c546d9e1483b1d3e62c54e4f5389d6b1
floretinapopa <floretinapopa67@gmail.com>
2022-05-06 18:02:02 +0300

    Initial setup

---
+++ src/MainPage.jsx
@@ -0,0 +1,12 @@
+// MainPage.jsx
+import React from 'react';
+
+function MainPage() {
+    return ( 
+    <div class="mt-10 text-blue-600 text-3xl font-bold" id="MainPage">
+        Main Page
+    </div>
+    );
+}
+
+export default MainPage;
